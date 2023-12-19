8a8443dd4363a538822b841721669b9b1b5f9ccd
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2023-12-19 23:25:27 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,6 @@ import streamlit.components.v1 as components
 # Set page title
 st.set_page_config(
     page_title="Brain Tumor Detection and Classification",
-    page_icon="🧠",
     initial_sidebar_state="expanded",
 )
 st.write('<style>div.row-widget.stMarkdown { font-size: 24px; }</style>', unsafe_allow_html=True)
@@ -25,7 +24,7 @@ components.html(
             top: 0px;
             right: 25%;
             position: fixed;
-            background: -webkit-linear-gradient(0.25turn,blue,white);
+            background: -webkit-linear-gradient(0.25turn,pink,white);
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
         }
@@ -40,23 +39,19 @@ components.html(
 
 
 def page_layout():
-    st.write("BrainVision is an app that combines various ML models into one in order to determine if you have a disease, using CNN and MRIs of the patients. The app uses advanced algorithms to diagnose various diseases related to brain")
+    st.write("Brain Tumor Check is an app developed by Students Of SVEC that takes an image as input and predicts the type of tumor, using CNN and MRIs of the patients. This Web app uses advanced algorithms to diagnose various tumors and classify them into brain")
     st.write("Developed By SVEC")
     st.markdown("## Benefits:")
     st.write("- Fast and accurate diagnosis of diseases")
     st.write("- Non-invasive and painless diagnosis using MRI")
     st.write("- Accessible from anywhere, anytime")
-    st.markdown("## Why is our app unique?")
-    st.write("- BrainVision combines multiple ML models into one app")
+    st.markdown("## Why to use our App?")
+    st.write("- Our app combines multiple ML models into one app")
     st.write("- The app uses CNN on MRI imagery to diagnose diseases")
-    st.write("- BrainVision uses advanced algorithms to provide fast and accurate diagnosis")
-    st.markdown("## Relevance:")
-    st.write("- Our App can diagnose various diseases, such as brain tumor")
-    st.write("- The app can be used by doctors, hospitals, and patients")
-    st.write("- BrainVision can improve the accuracy and speed of disease diagnosis")
+    st.write("- It uses advanced algorithms to provide fast and accurate diagnosis")
     st.markdown("## Uses:")
-    st.write("- Hospitals and clinics can use BrainVision to diagnose diseases more quickly")
-    st.write("- Patients can use BrainVision to get a quick and accurate diagnosis without the need for invasive procedures")
+    st.write("- Hospitals and clinics can use our app to diagnose diseases more quickly")
+    st.write("- Patients can use our app to get a quick and accurate diagnosis without the need for invasive procedures")
 
     
 # Render page layout
